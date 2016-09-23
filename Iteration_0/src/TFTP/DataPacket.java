91db91efc0e8e50c5237444f2a365f4b10009a38
ShasRanasinghe <shasthraranasinghe@hotmail.com>
2016-09-23 13:44:13 -0500

    added getBlockNumber method to dataPacket
    added getBlockNumber method
    added an invalid zero check for the data packets

--- Iteration_0/src/TFTP/DataPacket.java
+++ Iteration_0/src/TFTP/DataPacket.java
@@ -5,14 +5,26 @@ import java.io.*;
 public class DataPacket {
 	
 	private ByteArrayOutputStream byteStream;
+	private int blockNumber;
 	
 	public boolean validate(byte[] data){
-		if(data.length > 516) return false;
-		else if (data[0]!=0) return false;
-	    else if (data[1]!=3) return false;
+		byte[] temp = new byte[2];
+		if(data[0]!=0) return false;
+		else if (data[1]!=3) return false;
+	    else if (data.length > 516) return false;
+	    else if(hasZeros(data)) return false;
+		temp[0] = data[2]; temp[1] = data[3];
+		ByteBuffer buf = ByteBuffer.wrap(temp);
+		blockNumber = buf.getInt();
 		return true;
 	}
 	
+	
+	/**
+	 * @author shast
+	 * @param blockNum
+	 * @return
+	 */
 	public byte[] createPacketData(byte[] blockNum){
 		ArrayList<byte[]> tempArray = new ArrayList<byte[]>();
 		ByteBuffer buffer = ByteBuffer.wrap(blockNum);
@@ -31,5 +43,16 @@ public class DataPacket {
 			e.printStackTrace();
 		}
 		return byteStream.toByteArray();
+	}
+	
+	public int getBlockNumber(){
+		return blockNumber;
+	}
+	
+	private boolean hasZeros(byte[] array){	
+		for(int j = 4;j<array.length;j++) { 
+            if (array[j] == 0) return false;
+        }
+		return true;
 	}
 }
