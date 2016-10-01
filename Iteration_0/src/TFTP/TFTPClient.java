26e88424abbc9c62ee3bcf95526f850623abbb65
mohamed zalat <mohamedzalat@cmail.carleton.ca>
2016-10-01 01:12:46 -0400

    Fixed client writing to the server and made the client exit on socket timeout for now

--- Iteration_0/src/TFTP/TFTPClient.java
+++ Iteration_0/src/TFTP/TFTPClient.java
@@ -9,6 +9,7 @@ import static TFTPPackets.TFTPPacket.MAX_SIZE;
 
 import java.io.*;
 import java.net.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 import Exceptions.PacketOverflowException;
@@ -30,6 +31,8 @@ private TFTPReader tftpReader;
 private TFTPWriter tftpWriter;
 private static Mode run;
 private boolean firstTime;
+private static boolean verbose;
+private static final int testModeSendPort = 23;
 
 // we can run in normal (send directly to server) or test
 // (send to simulator) mode
@@ -43,6 +46,7 @@ public TFTPClient()
       // port on the local host machine. This socket will be used to
       // send and receive UDP Datagram packets.
       sendReceiveSocket = new DatagramSocket();
+      sendReceiveSocket.setSoTimeout(10000);
    } catch (SocketException se) {   // Can't create the socket.
       se.printStackTrace();
       System.exit(1);
@@ -69,7 +73,7 @@ public void sendRequest(Scanner sc) throws PacketOverflowException, FileNotFound
    
    boolean done = false;
    while(!done){
-	 System.out.println("Enter R for a read request and W for a write request");
+	 System.out.println("Enter R for a read request, W for a write request and Q to quit");
 	 String cmd = sc.nextLine();
 	 
 	 if(cmd.equals("W")){
@@ -77,9 +81,18 @@ public void sendRequest(Scanner sc) throws PacketOverflowException, FileNotFound
 
      
      // next we have a file name
- 	System.out.println("Enter file name");
- 	filename = sc.nextLine();
- 	
+	for(;;){
+ 		System.out.println("Enter file name");
+ 		filename = sc.nextLine();
+ 		if(new File (filePath + "\\" + filename).isFile()){
+                	//is the path was provided finish
+                	System.out.println("You have entered a valid file name\n");
+                	break;
+            }else{
+                //if the directory does not exist, ask for an input again
+                System.out.println("Invalid file name\nPlease Try Again.");
+            }
+	}
  		packet = new WRQPacket(filename, RRQWRQPacketCommon.Mode.NETASCII);
  		data = packet.getByteArray();
  	done = true;
@@ -100,6 +113,14 @@ public void sendRequest(Scanner sc) throws PacketOverflowException, FileNotFound
 		 	RRQPacket rPacket = (RRQPacket) packet;
 		 	 tftpWriter = new TFTPWriter(new File(filePath + rPacket.getFilename()).getPath(),false);
 	 }
+	 else if(cmd.equals("Q")) {
+		 System.out.println("Client: Closing socket and exiting.");
+		 	
+		     // next we have a file name -- let's just pick one
+		 	sc.close();
+		 	sendReceiveSocket.close();
+		 	System.exit(0);
+	 }
    }
      try {
         sendPacket = new DatagramPacket(data, data.length,
@@ -108,16 +129,16 @@ public void sendRequest(Scanner sc) throws PacketOverflowException, FileNotFound
         e.printStackTrace();
         System.exit(1);
      }
-
-     System.out.println("Client: sending packet.");
-     System.out.println("To host: " + sendPacket.getAddress());
-     System.out.println("Destination host port: " + sendPacket.getPort());
-     System.out.println("Length: " + data.length);
-     System.out.println("Containing: ");
-     for (j=0;j<data.length;j++) {
-         System.out.println("byte " + j + " " + data[j]);
+     if(verbose){
+	     System.out.println("Client: sending packet.");
+	     System.out.println("To host: " + sendPacket.getAddress());
+	     System.out.println("Destination host port: " + sendPacket.getPort());
+	     System.out.println("Length: " + data.length);
+	     System.out.println("Containing: ");
+	     for (j=0;j<data.length;j++) {
+	         System.out.println("byte " + j + " " + data[j]);
+	     }
      }
-	 
      // Form a String from the byte array, and print the string.
      String sending = new String(data,0,data.length);
      System.out.println(sending);
@@ -154,22 +175,28 @@ private void sendReceivePacket(Scanner sc){
         //Create byte array of proper size
         data = new byte[receivePacket.getLength()];
         System.arraycopy(dataBuffer, 0, data, 0, data.length);
-        System.out.println("\nServer: Received Packet.");
 
         // Process the received datagram.
-        System.out.println("Client: Packet received:");
-        System.out.println("From host: " + receivePacket.getAddress());
-        System.out.println("Host port: " + receivePacket.getPort());
-        int len = receivePacket.getLength();
-        System.out.println("Length: " + len);
-        System.out.println("Containing: ");
+       
+        if(verbose){
+        	System.out.println("\nClient: Packet received:");
+        	System.out.println("From host: " + receivePacket.getAddress());
+        	System.out.println("Host port: " + receivePacket.getPort());
+        	int len = receivePacket.getLength();
+        	System.out.println("Length: " + len);
+        	System.out.println("Containing: ");
+        	System.out.println(new String(Arrays.copyOfRange(data,0,len)));
+        	System.out.println("Byte Array: " + Arrays.toString(Arrays.copyOfRange(data,0,len))+"\n");
+        }
 
         //Get opcode
         Opcode opcode = Opcode.asEnum((int) data[1]);
 
         
         if(opcode == Opcode.DATA){
-            System.out.println("Opcode: DATA");
+        	if(verbose){
+        		System.out.println("Opcode: DATA");
+        	}
             //create/validate data
             DataPacket dataPacket = new DataPacket(data);
             //write the data you just received
@@ -177,25 +204,66 @@ private void sendReceivePacket(Scanner sc){
             //create an ack packet from corresponding block number
             ACKPacket ackPacket = new ACKPacket(dataPacket.getBlockNumber());
             //create and send ack packet
+            if(run == Mode.NORMAL)
             sendPacket = new DatagramPacket(ackPacket.getByteArray(), ackPacket.getByteArray().length,
             receivePacket.getAddress(), receivePacket.getPort());
+            else
+            	sendPacket = new DatagramPacket(ackPacket.getByteArray(), ackPacket.getByteArray().length,
+                        receivePacket.getAddress(), testModeSendPort);
+            
+            if(verbose){
+            	System.out.println("Client: Sending packet");
+            	System.out.println("To host: " + sendPacket.getAddress());
+            	System.out.println("Destination host port: " + sendPacket.getPort());
+            	int length = sendPacket.getLength();
+            	System.out.println("Length: " + length);
+            	System.out.println("Byte Array: " + Arrays.toString(sendPacket.getData()));
+            }
+            
             sendReceiveSocket.send(sendPacket);
-            if(dataPacket.getData().length < 512) firstTime = true;
+            if(dataPacket.getData().length < 512) {
+            	System.out.println("file sent.");
+            	firstTime = true;
+            }
             
         }
         else if(opcode == Opcode.ACK){
-            System.out.println("Opcode: ACK");
+        	if(verbose){
+        		System.out.println("Opcode: ACK");
+        	}
             ACKPacket ackPacket = new ACKPacket(data);
             //send next block of file until there are no more blocks
-            if(ackPacket.getBlockNumber() < tftpReader.getNumberOfBlocks()){
+            if(ackPacket.getBlockNumber() <= tftpReader.getNumberOfBlocks()){
                 DataPacket dataPacket = new DataPacket(ackPacket.getBlockNumber() + 1, tftpReader.getFileBlock(ackPacket.getBlockNumber() + 1));
-                sendPacket = new DatagramPacket(dataPacket.getByteArray(), dataPacket.getByteArray().length,
-                receivePacket.getAddress(), receivePacket.getPort());
-                sendReceiveSocket.send(sendPacket);
+                
+                if(run == Mode.NORMAL)
+                	sendPacket = new DatagramPacket(dataPacket.getByteArray(), dataPacket.getByteArray().length,
+                			receivePacket.getAddress(), receivePacket.getPort());
+                else
+                	sendPacket = new DatagramPacket(dataPacket.getByteArray(), dataPacket.getByteArray().length,
+                            receivePacket.getAddress(), testModeSendPort);
+                if(verbose){
+                	System.out.println("Client: Sending packet");
+                	System.out.println("To host: " + sendPacket.getAddress());
+                	System.out.println("Destination host port: " + sendPacket.getPort());
+                	int length = sendPacket.getLength();
+                	System.out.println("Length: " + length);
+                	System.out.println("Byte Array: " + Arrays.toString(sendPacket.getData()));
+                }
+                
+                try{
+                	sendReceiveSocket.send(sendPacket);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
-            else if(ackPacket.getBlockNumber() == tftpReader.getNumberOfBlocks()) firstTime = true;
+            else if(ackPacket.getBlockNumber() == tftpReader.getNumberOfBlocks() + 1){
+            	firstTime = true;
+            	System.out.println("file received.");
+            }
         }
     } catch (Exception e) {
+        System.exit(0);
         e.printStackTrace();
     }
 }
@@ -203,10 +271,44 @@ private void sendReceivePacket(Scanner sc){
 public static void main(String args[]) 
 {
 	Scanner in = new Scanner(System.in);
-	System.out.println("Enter Directory Path");
-	filePath = in.nextLine();
-	System.out.println("You have entered the Directory Path");
-	filePath += "\\";
+	System.out.println("Enter the Directory Path:");
+    System.out.println("Type \"DEFAULT\" to use the relative directory or Enter the filepath of the directory");
+    
+    for(;;){
+        String userInput = in.nextLine();
+        if(userInput.equals("DEFAULT")){
+            //if default print the dir and finish
+            System.out.println("You are now in: " + System.getProperty("user.dir") + "\\Client");
+            filePath = System.getProperty("user.dir") + "\\Client" + "\\";
+            break;
+        }else{
+            if(new File (userInput).isDirectory()){
+                //if the path was provided finish
+                filePath = userInput + "\\";
+                System.out.println("You have entered a valid Directory Path\n");
+                break;
+            }else{
+                //if the directory does not exist, ask for an input again
+                System.out.println("Invalid Directory\nPlease Try Again.");
+            }
+        }
+    }
+    
+	String userInput;
+    for(;;){
+        //request user for verbose or quiet mode
+        System.out.println("Verbose(Y/N)?");
+        userInput = in.nextLine();
+        if(userInput.equals("Y")){
+            verbose = true;
+            System.out.println("You have chosen Verbose mode");
+            break;
+        }else if(userInput.equals("N")){
+            verbose = false;
+            System.out.println("You have chosen Quiet mode");
+            break;
+        }//if input is invalid, ask again
+    }
 	boolean done = false;
 	
 	while(!done){
@@ -225,8 +327,14 @@ public static void main(String args[])
 	}
 	TFTPClient c = new TFTPClient();
 	
-	while(true)
-	c.sendReceivePacket(in);
+	while(true) {
+		try {
+			c.sendReceivePacket(in);
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
 }
 
 }
