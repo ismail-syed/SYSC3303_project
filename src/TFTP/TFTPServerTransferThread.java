72a3c79430ac80507135a3719c51116278a239ff
mohamed zalat <mohamedzalat@cmail.carleton.ca>
2016-10-15 02:22:20 -0400

    Fixed bug: Server now waits for last ACK

--- src/TFTP/TFTPServerTransferThread.java
+++ src/TFTP/TFTPServerTransferThread.java
@@ -130,8 +130,8 @@ public class TFTPServerTransferThread implements Runnable {
                 }
                 previousBlockNumber = ackPacket.getBlockNumber()+1;
                 //Send next block of file until there are no more blocks
-                if (ackPacket.getBlockNumber() <= tftpReader.getNumberOfBlocks()) {
-                    tftpPacket = new DataPacket(previousBlockNumber, tftpReader.getFileBlock(previousBlockNumber));
+                if (ackPacket.getBlockNumber() < tftpReader.getNumberOfBlocks()) {
+                    tftpPacket = new DataPacket(ackPacket.getBlockNumber() + 1, tftpReader.getFileBlock(ackPacket.getBlockNumber() + 1));
                     System.out.println("Sending DATA with block " + (previousBlockNumber));
                 }
                 if (ackPacket.getBlockNumber() == tftpReader.getNumberOfBlocks() || tftpReader.getNumberOfBlocks() == 0) {
